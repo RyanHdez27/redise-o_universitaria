9e276b8b60ca37125fc5a0965d55ca86df913570
Ryan Hernandez <ikkiamagiri11@gmail.com>
2025-04-10 18:58:46 -0500

    login v2

--- app.py
+++ app.py
@@ -12,5 +12,9 @@ def portal():
 def login():
     return render_template("login.html")
 
+@app.route('/ingresar')
+def ingresar():
+    return render_template("login_ll.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
